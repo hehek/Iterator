9a7bece80d3e2e4934801c803cf6eecef4c55543
Anna Pavlovskaya <annvsann@gmail.com>
2019-03-15 22:17:37 +0200

    WHOA

---
+++ Iterator/Iterator/ConcreteIterator.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iterator
+{
+    class ConcreteIterator: Iterator
+    {
+        private ConcreteDatabase _database;
+        private int _current = 0;
+
+        // Constructor
+        public ConcreteIterator(ConcreteDatabase database)
+        {
+           _database = database;
+        }
+
+        // Gets first iteration item
+        public override object First()
+        {
+            return _database[0];
+        }
+
+        // Gets next iteration item
+        public override object Next()
+        {
+            
+            if (_current < _database.Count - 1)
+            {
+                _current++;               
+            }
+            else
+            {
+                _current = 0;
+            }            
+            return _database[_current];
+        }
+
+        public override object Prev()
+        {         
+            if (_current > 0)
+            {
+                _current--;               
+            }
+            else
+            {
+                _current = _database.Count - 1;              
+            }
+            return _database[_current];
+        }
+
+
+        // Gets current iteration item
+        public override object CurrentItem()
+        {
+            return _database[_current];
+        }
+
+        // Gets whether iterations are complete
+        public override bool IsDone()
+        {
+            return _current >= _database.Count;
+        }
+    }
+}
+
--- Iterator/Program.cs
+++ Iterator/Program.cs
@@ -10,6 +10,55 @@ namespace Iterator
     {
         static void Main(string[] args)
         {
+
+            ConcreteDatabase a = new ConcreteDatabase(5);        
+
+            Iterator i = a.CreateIterator();
+            object item = null;
+
+            Console.WriteLine("To enter the iterator press \"Right Arrow\"");
+            Console.WriteLine("Next item:" +" \"Right Arrow\"".PadLeft(22));
+            Console.WriteLine("Previous item:" + " \"Left Arrow\"".PadLeft(17));
+            Console.WriteLine("Exit the iterator:"+" \"N\"".PadLeft(2));
+            Console.WriteLine("Exit the program:"+" \"Q\"".PadLeft(5));
+
+            while (true)
+            {
+                ConsoleKey key = Console.ReadKey(true).Key;
+
+                    //Convert.ToInt16(Console.ReadKey(true).KeyChar.ToString()) - 1;
+
+                if (key == ConsoleKey.Q)
+                {
+                    break;
+                }
+                if (key == ConsoleKey.N)
+                {
+                    item = null;
+                    Console.WriteLine("\nWhoa!");
+                }
+                else if (key == ConsoleKey.LeftArrow && item !=null)
+                {                   
+                        item = i.Prev();
+                        Console.Write(item+", ");                                      
+                }
+                else if (key == ConsoleKey.RightArrow)
+                {
+                    if (item == null)
+                    {
+                        item = i.First();
+                    }
+                    else
+                    {
+                        item = i.Next();
+                    }
+                    Console.Write(item+", ");
+                }
+            }           
+         
+           
+
+
         }
     }
 }
